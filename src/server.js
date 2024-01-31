f93df2c64a2730a1ec1c3c40034fd260a7f9960a
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-01-30 23:35:23 -0300

    dynamic elements on the study page

--- src/server.js
+++ src/server.js
@@ -15,8 +15,8 @@ const proffys = [
     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4",
     whatsapp: "899987654534", 
     bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    subject: "Química", 
-    cost: "R$20,00", 
+    subject: "Matemática", 
+    cost: "R$25,00", 
     weekday: [1], 
     time_from: [720], 
     time_to: [1220]
@@ -27,7 +27,7 @@ function pageLanding(req, res){
   return res.render("index.html")
 }
 function pageStudy(req, res){
-  return res.render("study.html")
+  return res.render("study.html", { proffys })
 }
 function pageGiveClasses(req, res){
   return res.render("give-classes.html")
@@ -45,7 +45,7 @@ nunjucks.configure('src/views', {
 server
   .use(express.static("public"))
   .get("/", pageLanding)
-  .get("/", pageStudy)
-  .get("/", pageGiveClasses)
+  .get("/study", pageStudy)
+  .get("/give-classes", pageGiveClasses)
   
   .listen(5500);
