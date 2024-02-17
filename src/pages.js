75227c58c002158f02da2cb2486804ea7b68ae4f
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-02-16 23:31:58 -0300

    converter hour to minutes function

--- src/pages.js
+++ src/pages.js
@@ -1,11 +1,32 @@
-import { subjects, weekdays, getSubject} from './utils/format.js'
+import Database from "./database/db.js";
+
+import { subjects, weekdays, getSubject, convertHoursToMinutes } from "./utils/format.js";
 
 function pageLanding(req, res) {
   return res.render("index.html");
 }
 function pageStudy(req, res) {
   const filters = req.query;
-  return res.render("study.html", { proffys, filters, subjects, weekdays });
+
+  if (!filters.subject || !filters.weekday || !filters.time) {
+    return res.render("study.html", { filters, subjects, weekdays });
+  }
+
+  const timeToMinutes = convertHoursToMinutes(filters.time)
+
+  const query = `
+    SELECT classes.*,proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE EXISTS(
+      SELECT class_schedule.*
+      FROM class_schedule
+      WHERE class_schedule.class_id = classes.id
+      AND class_schedule.weekday = ${filters.weekday}
+      AND class_schedule.time_from <= ${timeToMinutes}
+      AND class_schedule.time_to > ${timeToMinutes}
+    )
+  `;
 }
 function pageGiveClasses(req, res) {
   const data = req.query;
@@ -20,4 +41,4 @@ function pageGiveClasses(req, res) {
   return res.render("give-classes.html", { subjects, weekdays });
 }
 
-export {pageLanding, pageStudy, pageGiveClasses}
+export { pageLanding, pageStudy, pageGiveClasses };
