2aefef5cfb275125e9b5bf17ee396ea0286dba3d
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-02-17 19:25:55 -0300

    saveClasses function to register new teachers

--- src/pages.js
+++ src/pages.js
@@ -1,6 +1,13 @@
 import Database from "./database/db.js";
+import createProffy from './database/createProffy.cjs'
 
-import { subjects, weekdays, getSubject, convertHoursToMinutes } from "./utils/format.js";
+
+import {
+  subjects,
+  weekdays,
+  getSubject,
+  convertHoursToMinutes,
+} from "./utils/format.js";
 
 function pageLanding(req, res) {
   return res.render("index.html");
@@ -12,7 +19,7 @@ async function pageStudy(req, res) {
     return res.render("study.html", { filters, subjects, weekdays });
   }
 
-  const timeToMinutes = convertHoursToMinutes(filters.time)
+  const timeToMinutes = convertHoursToMinutes(filters.time);
 
   const query = `
     SELECT classes.*,proffys.*
@@ -29,26 +36,59 @@ async function pageStudy(req, res) {
     AND classes.subject = "${filters.subject}"
   `;
   try {
-    const db = await Database
-    const proffys = await db.all(query)
+    const db = await Database;
+    const proffys = await db.all(query);
 
-    return res.render('study.html', { proffys, subjects, filters, weekdays })
+    proffys.map((proffy) => {
+      proffy.subject = getSubject(proffy.subject)
+    })
 
+    return res.render("study.html", { proffys, subjects, filters, weekdays });
   } catch (error) {
     console.log(error);
   }
 }
 function pageGiveClasses(req, res) {
-  const data = req.query;
-
-  const isNotEmpty = Object.keys(data).length > 0;
-  if (isNotEmpty) {
-    data.subject = getSubject(data.subject);
-    proffys.push(data);
-    return res.redirect("/study");
-  }
-
   return res.render("give-classes.html", { subjects, weekdays });
 }
 
-export { pageLanding, pageStudy, pageGiveClasses };
+async function saveClasses(req, res) {
+  
+  const proffyValue = {
+    name: req.body.name,
+    avatar: req.body.avatar,
+    whatsapp: req.body.whatsapp,
+    bio: req.body.bio
+  }
+
+  const classValue = {
+    subject: req.body.subject,
+    cost: req.body.cost
+  }
+
+  const classScheduleValues = req.body.weekday.map((weekday, index) => {
+    return {
+      weekday,
+      time_from: convertHoursToMinutes(req.body.time_from[index]),
+      time_to: convertHoursToMinutes(req.body.time_to[index])
+    }
+  })
+
+  try {
+    const db = await Database
+    await createProffy(db, { proffyValue, classValue, classScheduleValues})
+
+    let queryString = "?subject=" + req.body.subject
+    queryString += "&weekday=" + req.body.weekday[0]
+    queryString += "&time=" + req.body.time_from[0]
+
+    return res.redirect("/study" + queryString);
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+  
+}
+
+export { pageLanding, pageStudy, pageGiveClasses, saveClasses };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 const server = express();
 import nunjucks from "nunjucks";
-import {pageLanding, pageStudy, pageGiveClasses} from './pages.js'
+import {pageLanding, pageStudy, pageGiveClasses, saveClasses} from './pages.js'
 
 nunjucks.configure("src/views", {
   express: server,
@@ -9,9 +9,10 @@ nunjucks.configure("src/views", {
 });
 
 server
+.use(express.urlencoded({ extended: true }))
   .use(express.static("public"))
   .get("/", pageLanding)
   .get("/study", pageStudy)
   .get("/give-classes", pageGiveClasses)
-
+  .post("/save-classes", saveClasses)
   .listen(5500);
